599a32dce4990159cf6457e564685b26857fb2de
Brenda <brenda.aguiar@icomp.ufam.edu.br>
2019-03-14 09:15:10 -0400

    Correção da responsividade das paginas dos projetos
    --Uma pequena mudança no layout no AboutProjects

--- frontend/src/template/AboutProject/AboutProject.jsx
+++ frontend/src/template/AboutProject/AboutProject.jsx
@@ -3,35 +3,53 @@ import './AboutProject.css'
 
 
 export default props => (
-    <div class="container">
+    <div class="container" >
         <div className="row">
-            <div class="col-5">
+            <div class="col-sm">
                 <h2>Title</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
                 </p>
             </div>
-            <div class="col-7">
+            <div class="col-sm mt-5">
                 <img src="https://www.lamborghini.com/sites/it-en/files/DAM/it/models_gateway/blocks/special.png" alt="foto" className ="img-fluid" />
             </div>
-            <div class="w-100"></div>
-            <div class="col-6">
-                <img src="https://www.lamborghini.com/sites/it-en/files/DAM/it/models_gateway/blocks/special.png" alt="foto" className ="img-fluid" />
-            </div>
-            <div class="col-5">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
-                </p>
-            </div>
-            <div class="w-100"></div>
-            <div class="col-5">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
-                </p>
-            </div>
-            <div class="col-7">
-                <img src="https://www.lamborghini.com/sites/it-en/files/DAM/it/models_gateway/blocks/special.png" alt="foto" className ="img-fluid" />
+        </div>   
+         
+            <div className="row">
+                <div class="col-sm">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
+                    </p>
+                </div>
             </div>
            
-        </div>
+           <div className="row">
+                <div class="col-sm">
+                    <img src="https://www.lamborghini.com/sites/it-en/files/DAM/it/models_gateway/blocks/special.png" alt="foto" className ="img-fluid" />
+                </div>
+                <div class="col-sm">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
+                    </p>
+                </div>
+           </div>
+
+           <div className="row">
+                <div class="col-sm">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
+                    </p>
+                </div>
+           </div>
+           
+           <div className="row">
+                <div class="col-sm">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum est quis massa vulputate, eu efficitur ex volutpat. Aliquam erat volutpat. Sed ultrices dignissim risus ac commodo. Etiam rutrum ante non bibendum mattis. Cras quis iaculis ex, at facilisis risus. Ut in hendrerit nisi. Sed nisl nisi, fringilla at massa ut, pharetra porttitor sapien. Nunc vitae malesuada ex. Cras metus lectus, lobortis volutpat pretium et, laoreet at sem.
+                    </p>
+                </div>
+                <div class="col-sm">
+                    <img src="https://www.lamborghini.com/sites/it-en/files/DAM/it/models_gateway/blocks/special.png" alt="foto" className ="img-fluid" />
+                </div>
+           </div>
     </div>
+   
    
 )
 
