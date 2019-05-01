472a151bdc816f8a53f2d4d87257e9eb5a1f8b1a
Victor Augusto <vaas@icomp.ufam.edu.br>
2019-05-01 10:04:23 -0400

    Alterado o link da agenda do PET Computação

--- src/template/Diary/Diary.jsx
+++ src/template/Diary/Diary.jsx
@@ -3,7 +3,7 @@ import './Diary.css'
 
 import CardDiary, { Card } from './CardDiary'
 
-let url = "https://calendar.google.com/calendar/b/2?cid=cGV0Y29tcHV0YWNhb0BpY29tcC51ZmFtLmVkdS5icg"
+let url = "https://calendar.google.com/calendar/embed?src=petcomputacao%40icomp.ufam.edu.br&ctz=America%2FBoa_Vista"
 
 export default props => (
     <section className="diary container-fluid p-5">
@@ -15,16 +15,16 @@ export default props => (
         </div>
 
         <CardDiary>
-            <Card url={url} date="10/03" title="Visita a escola Estadual Judas Wilson Lima"/>
-            <Card url={url} date="18/03" title="Reunião Mensal do PET"/>
+            <Card url={url} date="10/03" title="Visita a escola Estadual Judas Wilson Lima" />
+            <Card url={url} date="18/03" title="Reunião Mensal do PET" />
         </CardDiary>
         <CardDiary>
-            <Card url={url} date="25/03" title="PET Café"/>
-            <Card url={url} date="10/04" title="Oficina de Programação C"/>
+            <Card url={url} date="25/03" title="PET Café" />
+            <Card url={url} date="10/04" title="Oficina de Programação C" />
         </CardDiary>
         <CardDiary>
-            <Card url={url} date="15/04" title="Inicio do Curso de Informática"/>
-            <Card url={url} date="25/04" title="Reunião Mensal do PET"/>
+            <Card url={url} date="15/04" title="Inicio do Curso de Informática" />
+            <Card url={url} date="25/04" title="Reunião Mensal do PET" />
         </CardDiary>
     </section>
 )
