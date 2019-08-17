0f049ba1a83f083ae3937dd3a6eacad730a85d73
Brenda Aguiar <brenda.aguiar@icomp.ufam.edu.br>
2019-08-17 13:11:04 -0400

    Algumas mudanças no layout da pagina dos petianos

--- src/components/Team/TeamItem/TeamItem.jsx
+++ src/components/Team/TeamItem/TeamItem.jsx
@@ -3,13 +3,15 @@ import './TeamItem.css'
 
 export default props => (
     <div className="d-flex flex-column align-items-center">
-        <img src={props.urlImg} alt={props.altImg} className="img-team rounded-circle" />
-
-        <h4 className="title-name-team mt-2">{props.name}</h4>
-
-        <a href={`mailto:${props.email}`} className="email-team ml-2 mt-2">
-            <i className="far fa-envelope"></i> {props.email}
+        <img src={props.urlImg} alt={props.altImg} className="img-team" />
+        
+        <figcaption className="figure-caption">{props.name}</figcaption>
+    
+        <figcaption className="figure-caption1">
+        <a href={`mailto:${props.email}`} className="email-team">
+           {props.email}
         </a>
+        </figcaption>
 
     </div>
 )
